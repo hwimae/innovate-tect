09320ed9d2b7599c3b3d656ec3974c4071a7bc28
Huy Mai <dnimer211@gmail.com>
2025-07-31 10:41:41 +0700

    demo2

--- views/Home.vue
+++ views/Home.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
-    <HeroSection />
+    <MainBodySection />
     <AboutSection />
     <ServicesSection />
     <FeaturesSection />
-    <WorkflowSection />
+    <SolutionSection />
     <FaqSection />
     <TestimonialsSection />
   </div>
 </template>
 
 <script>
-import HeroSection from '@/components/sections/HeroSection.vue'
+import MainBodySection from '@/components/sections/MainBodySection.vue'
 import AboutSection from '@/components/sections/AboutSection.vue'
 import ServicesSection from '@/components/sections/ServicesSection.vue'
 import FeaturesSection from '@/components/sections/FeaturesSection.vue'
-import WorkflowSection from '@/components/sections/WorkflowSection.vue'
+import SolutionSection from '@/components/sections/SolutionSection.vue'
 import FaqSection from '@/components/sections/FaqSection.vue'
 import TestimonialsSection from '@/components/sections/TestimonialsSection.vue'
 
 export default {
   name: 'Home',
   components: {
-    HeroSection,
+    MainBodySection,
     AboutSection,
     ServicesSection,
     FeaturesSection,
-    WorkflowSection,
+    SolutionSection,
     FaqSection,
     TestimonialsSection
   }
